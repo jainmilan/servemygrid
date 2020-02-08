facfc577c7114bf8892b7103bed2ad13af673872
Milan Jain <milanjain81@gmail.com>
2020-02-08 02:28:14 -0800

    v0.02

--- static/js/plot.js
+++ static/js/plot.js
@@ -69,6 +69,72 @@ function plotApplicationBox(appType) {
             .attr('src', 'static/images/icons/upload.png');
 }
 
+function getTable(buildingType) {
+    var serviceTable = d3.select('.servicePanel')
+                            .append('table')
+                                .attr('class', 'table');
+    var serviceTableHead = serviceTable
+                            .append('thead')
+                                .attr('class', 'thead-light')
+                            .append('tr');
+    
+    serviceTableHead
+        .append('th')
+            .attr('scope', 'col')
+            .attr('class', 'imageRow')
+        .append('img')
+            .attr('class', 'table-image')
+            .attr('src', 'static/images/icons/' + buildingType + '.png');
+    
+    serviceTableHead
+        .selectAll('th.headRow')
+        .data(['Curtailment', 'Frequency Regulation', 'Ramping', 'Reactive Power Control'])
+            .enter()
+        .append('th')
+            .attr('class', 'headRow')
+            .attr('scope', 'col')
+            .text(function(d){return d;});
+    
+    var serviceTableBody = serviceTable
+                            .append('tbody')
+                                .attr('class', 'tbody'); 
+}
+
+function getAppliancePanel(dataList, buildingType) {
+    dataList[0][buildingType].forEach(function(d){
+        var appType = Object.keys(d)[0];
+        plotApplicationBox(appType);                                  
+    });
+    
+    jQuery('.servicePanel').show();
+    d3.select(".servicePanel").selectAll("*").remove();
+    getTable(buildingType);
+    
+    jQuery('input[type=checkbox][name=applianceType]').on('change', function(){
+        if (jQuery(this).is(':checked')) {
+            var tr = d3.selectAll('.tbody')
+                        .append('tr')
+                            .attr('class', 'tableRow')
+                            .attr('id', jQuery(this).val() + 'Row');
+                
+            tr.append('th')
+                .attr('scope', 'row')
+              .append('img')
+                .attr('class', 'table-image')
+                .attr('src', 'static/images/icons/' + jQuery(this).val() + '.png');
+            
+            tr.selectAll('.tableRow')
+                .data([1, 1, 1, 1])
+                .enter()
+              .append('td')
+                .attr('class', 'cell')
+                .text(function(d){return d;});
+        } else {
+            d3.select('#' + jQuery(this).val() + 'Row').selectAll("*").remove();
+        }               
+    });
+}
+
 jQuery(document).ready(function(){
     var currentDiv = "buildingType";
     var dataList;
@@ -118,11 +184,9 @@ jQuery(document).ready(function(){
 
             d3.select("#applianceList").selectAll("*").remove();
             var buildingType = jQuery('input[name="commercialType"]:checked').val();
-            dataList[0][buildingType].forEach(function(d){
-                var appType = Object.keys(d)[0];
-                plotApplicationBox(appType);
-            });
+            getAppliancePanel(dataList, buildingType);
         }
+        
         jQuery('.commercialType').hide();
         jQuery('.' + currentDiv).show();
     });
@@ -139,6 +203,7 @@ jQuery(document).ready(function(){
         }
         jQuery('.' + currentDiv).show();
         jQuery('.appliancePanel').hide();
+        jQuery('.servicePanel').hide();
     });
     
     jQuery('input[type=radio][name=residentialType]').on('change', function(){
@@ -147,10 +212,7 @@ jQuery(document).ready(function(){
         
         d3.select("#applianceList").selectAll("*").remove();
         var buildingType = jQuery('input[name="residentialType"]:checked').val();
-        dataList[0][buildingType].forEach(function(d){
-            var appType = Object.keys(d)[0];
-            plotApplicationBox(appType);
-        });
+        getAppliancePanel(dataList, buildingType);
     });
     
     jQuery('input[type=radio][name=officeType]').on('change', function(){
@@ -159,10 +221,7 @@ jQuery(document).ready(function(){
         
         d3.select("#applianceList").selectAll("*").remove();
         var buildingType = jQuery('input[name="officeType"]:checked').val();
-        dataList[0][buildingType].forEach(function(d){
-            var appType = Object.keys(d)[0];
-            plotApplicationBox(appType);
-        });
+        getAppliancePanel(dataList, buildingType);
     });
     
     jQuery('input[type=radio][name=healthcareType]').on('change', function(){
@@ -171,10 +230,7 @@ jQuery(document).ready(function(){
         
         d3.select("#applianceList").selectAll("*").remove();
         var buildingType = jQuery('input[name="healthcareType"]:checked').val();
-        dataList[0][buildingType].forEach(function(d){
-            var appType = Object.keys(d)[0];
-            plotApplicationBox(appType);
-        });
+        getAppliancePanel(dataList, buildingType);
     });
     
     jQuery('input[type=radio][name=schoolType]').on('change', function(){
@@ -183,10 +239,7 @@ jQuery(document).ready(function(){
         
         d3.select("#applianceList").selectAll("*").remove();
         var buildingType = jQuery('input[name="schoolType"]:checked').val();
-        dataList[0][buildingType].forEach(function(d){
-            var appType = Object.keys(d)[0];
-            plotApplicationBox(appType);
-        });
+        getAppliancePanel(dataList, buildingType);
     });
     
     jQuery('input[type=radio][name=hotelType]').on('change', function(){
@@ -195,10 +248,7 @@ jQuery(document).ready(function(){
         
         d3.select("#applianceList").selectAll("*").remove();
         var buildingType = jQuery('input[name="hotelType"]:checked').val();
-        dataList[0][buildingType].forEach(function(d){
-            var appType = Object.keys(d)[0];
-            plotApplicationBox(appType);
-        });
+        getAppliancePanel(dataList, buildingType);
     });
     
     jQuery('input[type=radio][name=restaurantType]').on('change', function(){
@@ -207,10 +257,7 @@ jQuery(document).ready(function(){
         
         d3.select("#applianceList").selectAll("*").remove();
         var buildingType = jQuery('input[name="restaurantType"]:checked').val();
-        dataList[0][buildingType].forEach(function(d){
-            var appType = Object.keys(d)[0];
-            plotApplicationBox(appType);
-        });
+        getAppliancePanel(dataList, buildingType);
     });
 
     jQuery('input[type=radio][name=shopType]').on('change', function(){
@@ -219,10 +266,7 @@ jQuery(document).ready(function(){
         
         d3.select("#applianceList").selectAll("*").remove();
         var buildingType = jQuery('input[name="shopType"]:checked').val();
-        dataList[0][buildingType].forEach(function(d){
-            var appType = Object.keys(d)[0];
-            plotApplicationBox(appType);
-        });
+        getAppliancePanel(dataList, buildingType);
     });
 });
 
