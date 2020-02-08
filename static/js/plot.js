25261652189b04b326928d04b59a10c3e0418cec
Milan Jain <milanjain81@gmail.com>
2020-02-07 20:47:19 -0800

    Data driven appliance selection

--- static/js/plot.js
+++ static/js/plot.js
@@ -1,6 +1,81 @@
+function plotApplicationBox(appType) {
+    var selectionBox = d3.select('#applianceList')
+                        .append('div')
+                            .attr('class', 'col-md-5 form-check form-check-inline selectionBox');
+
+    var imageBox = selectionBox
+                    .append('div')
+                        .attr('class', 'col-md-8 imageBox');
+
+    imageBox.append('input')
+        .attr('class', 'form-check-input')
+        .attr('type', 'checkbox')
+        .attr('name', 'applianceType')
+        .attr('id', appType)
+        .attr('value', appType);
+
+    var figure = imageBox.append('label')
+                            .attr('class', 'form-check-label leaf')
+                            .attr('for', appType)
+                        .append('figure');
+    figure.append('img')
+            .attr('class', 'label-image')
+            .attr('src', 'static/images/icons/' + appType + '.png');
+
+    figure.append('figcaption')
+            .attr('class', 'tag')
+            .text(function(){
+                var output = "Specialized Lab Equipment";
+                if (appType == "hvac") {
+                    output = "HVAC";
+                } else if (appType == "lighting") {
+                    output = "Lighting";
+                } else if (appType == "desktops") {
+                    output = "Desktops";
+                } else if (appType == "laptops") {
+                    output = "Laptops";
+                } else if (appType == "monitors") {
+                    output = "Monitors";
+                } else if (appType == "refrigerators") {
+                    output = "Refrigerators";
+                } else if (appType == "kitchenVent") {
+                    output = "Kitchen Ventillation";
+                } else if (appType == "videoDisplay") {
+                    output = "Video Display";
+                } else if (appType == "securitySystem") {
+                    output = "Security System";
+                } else if (appType == "medImaging") {
+                    output = "Medical Imaging Equipment";
+                }
+                return output;
+            });
+
+    var iconBox = selectionBox
+                    .append('div')
+                        .attr('class', 'col-md-4 iconBox');
+
+    var linkBox = iconBox
+                    .append('div')
+                        .attr('class', 'row linkIcon');
+    linkBox.append('img')
+            .attr('class', 'icon-image float-left')
+            .attr('src', 'static/images/icons/link.png');
+
+    var uploadBox = iconBox
+                    .append('div')
+                        .attr('class', 'row uploadIcon');
+    uploadBox.append('img')
+            .attr('class', 'icon-image float-left')
+            .attr('src', 'static/images/icons/upload.png');
+}
+
 jQuery(document).ready(function(){
     var currentDiv = "buildingType";
+    var dataList;
     
+    d3.csv("static/data/appList.csv").then(function(data){
+        dataList = data;
+    });
     var parentDict = {
         residentialType: "buildingType",
         commercialType: "buildingType",
@@ -38,7 +113,15 @@ jQuery(document).ready(function(){
             currentDiv = "shopType";
         } else {
             currentDiv = "commercialType";
+            jQuery('input:checkbox').prop('checked', false);
             jQuery('.appliancePanel').show();
+
+            d3.select("#applianceList").selectAll("*").remove();
+            var buildingType = jQuery('input[name="commercialType"]:checked').val();
+            dataList.forEach(function(d){
+                var appType = d[buildingType];
+                plotApplicationBox(appType);
+            });
         }
         jQuery('.commercialType').hide();
         jQuery('.' + currentDiv).show();
@@ -61,36 +144,85 @@ jQuery(document).ready(function(){
     jQuery('input[type=radio][name=residentialType]').on('change', function(){
         jQuery('input:checkbox').prop('checked', false);
         jQuery('.appliancePanel').show();
+        
+        d3.select("#applianceList").selectAll("*").remove();
+        var buildingType = jQuery('input[name="residentialType"]:checked').val();
+        dataList.forEach(function(d){
+            var appType = d[buildingType];
+            plotApplicationBox(appType);
+        });
     });
     
     jQuery('input[type=radio][name=officeType]').on('change', function(){
         jQuery('input:checkbox').prop('checked', false);
         jQuery('.appliancePanel').show();
+        
+        d3.select("#applianceList").selectAll("*").remove();
+        var buildingType = jQuery('input[name="officeType"]:checked').val();
+        dataList.forEach(function(d){
+            var appType = d[buildingType];
+            plotApplicationBox(appType);
+        });
     });
     
     jQuery('input[type=radio][name=healthcareType]').on('change', function(){
         jQuery('input:checkbox').prop('checked', false);
         jQuery('.appliancePanel').show();
+        
+        d3.select("#applianceList").selectAll("*").remove();
+        var buildingType = jQuery('input[name="healthcareType"]:checked').val();
+        dataList.forEach(function(d){
+            var appType = d[buildingType];
+            plotApplicationBox(appType);
+        });
     });
     
     jQuery('input[type=radio][name=schoolType]').on('change', function(){
         jQuery('input:checkbox').prop('checked', false);
         jQuery('.appliancePanel').show();
+        
+        d3.select("#applianceList").selectAll("*").remove();
+        var buildingType = jQuery('input[name="schoolType"]:checked').val();
+        dataList.forEach(function(d){
+            var appType = d[buildingType];
+            plotApplicationBox(appType);
+        });
     });
     
     jQuery('input[type=radio][name=hotelType]').on('change', function(){
         jQuery('input:checkbox').prop('checked', false);
         jQuery('.appliancePanel').show();
+        
+        d3.select("#applianceList").selectAll("*").remove();
+        var buildingType = jQuery('input[name="hotelType"]:checked').val();
+        dataList.forEach(function(d){
+            var appType = d[buildingType];
+            plotApplicationBox(appType);
+        });
     });
     
     jQuery('input[type=radio][name=restaurantType]').on('change', function(){
         jQuery('input:checkbox').prop('checked', false);
         jQuery('.appliancePanel').show();
+        
+        d3.select("#applianceList").selectAll("*").remove();
+        var buildingType = jQuery('input[name="restaurantType"]:checked').val();
+        dataList.forEach(function(d){
+            var appType = d[buildingType];
+            plotApplicationBox(appType);
+        });
     });
 
     jQuery('input[type=radio][name=shopType]').on('change', function(){
         jQuery('input:checkbox').prop('checked', false);
-        jQuery('.applianceType').show();
+        jQuery('.appliancePanel').show();
+        
+        d3.select("#applianceList").selectAll("*").remove();
+        var buildingType = jQuery('input[name="shopType"]:checked').val();
+        dataList.forEach(function(d){
+            var appType = d[buildingType];
+            plotApplicationBox(appType);
+        });
     });
 });
 
