5b8b501d8a448ad1d7c2e407c916ba3c4182401e
Milan Jain <milanjain81@gmail.com>
2020-03-12 17:56:18 -0700

    v0.2

--- static/js/plot.js
+++ static/js/plot.js
@@ -150,9 +150,113 @@ function getAppliancePanel(dataList, buildingType) {
     getTable(buildingType);
 }
 
+function printApplianceList(iType, id, buildingData) {
+    var label = "";
+    var appliances = [];
+    if (iType == "building") {
+        label = "Building Loads";
+        appliances = buildingData["appliances"];
+    } else if (iType == "floor") {
+        label = "Floor-" + id.split('F')[1].toString() + " Loads"
+        appliances = buildingData["floors"][id]["appliances"];
+    } else if (iType == "zone") {
+        label = "Floor-" + id.split('_')[0].split('F')[1].toString() + " Zone-" + id.split('_')[1].split('Z')[1].toString() + " Loads";
+        appliances = buildingData["floors"][id.split('_')[0]]["zones"][id.split('_')[1]];
+    }
+    
+    var applianceDiv = d3.select(".listOfAppliances");
+    applianceDiv.selectAll("*").remove();
+    
+    applianceDiv.append('label').text(label);
+    
+    var applianceList = applianceDiv.append('ul');
+    for (var i=0; i<appliances.length; i++) {
+        applianceList.append('li').text(appliances[i]);
+    }
+}
+
+function addFloor(f_no, floorPanel, nZones=1) {
+    var panel = floorPanel
+                    .append('div')
+                        .attr('class', 'col-sm-3 mb-4 f' + (f_no+1).toString())
+
+    panel
+        .append('label')
+            .text('F' + (f_no+1).toString())
+            .attr('class', 'col-sm-12 mx-0 px-0')
+
+    panel
+        .append('div')
+            .attr('class', 'col-sm-12 mx-0 px-0')
+        .append('input')
+            .attr('id', 'f' + (f_no+1).toString() + 'zones')
+            .attr('name', 'nzones')
+            .attr('class', 'form-control')
+            .attr('type', 'text')
+            .attr('value', nZones)
+            .attr('style', 'text-align:center;');
+}
+
+function generateTable(buildingModel, buildingData, max_nzones) {
+    buildingModel.selectAll('*').remove();
+    buildingTable = buildingModel.append('table')
+                        .attr('class', 'table table-sm table-bordered buildingMap')
+    
+    var tableBody = buildingTable.append('tbody');
+    
+    var nfloors = Object.keys(buildingData["floors"]).length;
+    for (var i=nfloors-1; i>=0; i--) {
+        floorRow = tableBody.append('tr')
+        nzones = Object.keys(buildingData["floors"]['F' + (i+1).toString()]["zones"]).length;
+        
+        var nzoneArraylen = nzones * Math.ceil(max_nzones % nzones);
+        
+        nzoneArray = new Array(nzones).fill(0);
+        for (var j=0; j<max_nzones; j++) {
+            nzoneArray[j%nzones] = nzoneArray[j%nzones] + 1;
+        }
+        
+        for (var j=0; j<nzones; j++) {
+            floorRow.append('td')
+                        .attr('colspan', nzoneArray[j])
+                        .attr('class', 'zones')
+                    .append('a')
+                        .attr('href', 'javascript:void(0);')
+                        .attr('id', 'F' + (i+1).toString() + '_Z' + (j+1).toString())
+                        .text('Z' + (j+1).toString())
+                    .on('click', function(data) {
+                        printApplianceList('zone', this.id, buildingData);
+                    });
+        }
+        
+        floorRow.append('td')
+                    .attr('class', 'floors')
+                .append('a')
+                    .attr('href', 'javascript:void(0);')
+                    .attr('id', 'F' + (i+1).toString())
+                    .text('F' + (i+1).toString())
+                .on('click', function(data) {
+                    printApplianceList('floor', this.id, buildingData);
+                });
+    }
+    
+    tableBody.append('tr').append('td')
+            .attr('scope', 'col')
+            .attr('colspan', max_nzones)
+            .attr('class', 'building')
+        .append('a')
+            .attr('href', 'javascript:void(0);')
+            .attr('id', 'B0')
+            .text("Building Loads")
+        .on('click', function(data) {
+            printApplianceList('building', this.id, buildingData);
+        });
+}
+
 jQuery(document).ready(function(){
     var currentDiv = "buildingType";
     var dataList;
+    var defaultBuilding;
     
     d3.json("static/data/sensorsList.json").then(function(data){
         dataList = data;
@@ -194,12 +298,12 @@ jQuery(document).ready(function(){
             currentDiv = "shopType";
         } else {
             currentDiv = "commercialType";
-            jQuery('input:checkbox').prop('checked', false);
-            jQuery('.appliancePanel').show();
+            // jQuery('input:checkbox').prop('checked', false);
+            // jQuery('.appliancePanel').show();
 
-            d3.select("#applianceList").selectAll("*").remove();
-            var buildingType = jQuery('input[name="commercialType"]:checked').val();
-            getAppliancePanel(dataList, buildingType);
+            // d3.select("#applianceList").selectAll("*").remove();
+            // var buildingType = jQuery('input[name="commercialType"]:checked').val();
+            // getAppliancePanel(dataList, buildingType);
         }
         
         jQuery('.commercialType').hide();
@@ -217,71 +321,173 @@ jQuery(document).ready(function(){
             jQuery('.backButton').hide();
         }
         jQuery('.' + currentDiv).show();
-        jQuery('.appliancePanel').hide();
-        jQuery('.servicePanel').hide();
+        jQuery('.configurationPanel').hide();
+        jQuery('.summaryPanel').hide();
+//        jQuery('.appliancePanel').hide();
+//        jQuery('.servicePanel').hide();
     });
-    
+    /*
     jQuery('input[type=radio][name=residentialType]').on('change', function(){
-        jQuery('input:checkbox').prop('checked', false);
-        jQuery('.appliancePanel').show();
+        // jQuery('input:checkbox').prop('checked', false);
+        // jQuery('.appliancePanel').show();
         
-        d3.select("#applianceList").selectAll("*").remove();
-        var buildingType = jQuery('input[name="residentialType"]:checked').val();
-        getAppliancePanel(dataList, buildingType);
+        // d3.select("#applianceList").selectAll("*").remove();
+        // var buildingType = jQuery('input[name="residentialType"]:checked').val();
+        // getAppliancePanel(dataList, buildingType);
     });
     
     jQuery('input[type=radio][name=officeType]').on('change', function(){
-        jQuery('input:checkbox').prop('checked', false);
-        jQuery('.appliancePanel').show();
+        // jQuery('input:checkbox').prop('checked', false);
+        // jQuery('.appliancePanel').show();
         
-        d3.select("#applianceList").selectAll("*").remove();
-        var buildingType = jQuery('input[name="officeType"]:checked').val();
-        getAppliancePanel(dataList, buildingType);
+        // d3.select("#applianceList").selectAll("*").remove();
+        // var buildingType = jQuery('input[name="officeType"]:checked').val();
+        // getAppliancePanel(dataList, buildingType);
     });
     
     jQuery('input[type=radio][name=healthcareType]').on('change', function(){
-        jQuery('input:checkbox').prop('checked', false);
-        jQuery('.appliancePanel').show();
+        // jQuery('input:checkbox').prop('checked', false);
+        // jQuery('.appliancePanel').show();
         
-        d3.select("#applianceList").selectAll("*").remove();
-        var buildingType = jQuery('input[name="healthcareType"]:checked').val();
-        getAppliancePanel(dataList, buildingType);
+        // d3.select("#applianceList").selectAll("*").remove();
+        // var buildingType = jQuery('input[name="healthcareType"]:checked').val();
+        // getAppliancePanel(dataList, buildingType);
     });
     
     jQuery('input[type=radio][name=schoolType]').on('change', function(){
-        jQuery('input:checkbox').prop('checked', false);
-        jQuery('.appliancePanel').show();
+        // jQuery('input:checkbox').prop('checked', false);
+        // jQuery('.appliancePanel').show();
         
-        d3.select("#applianceList").selectAll("*").remove();
-        var buildingType = jQuery('input[name="schoolType"]:checked').val();
-        getAppliancePanel(dataList, buildingType);
+        // d3.select("#applianceList").selectAll("*").remove();
+        // var buildingType = jQuery('input[name="schoolType"]:checked').val();
+        // getAppliancePanel(dataList, buildingType);
     });
     
     jQuery('input[type=radio][name=hotelType]').on('change', function(){
-        jQuery('input:checkbox').prop('checked', false);
-        jQuery('.appliancePanel').show();
+        // jQuery('input:checkbox').prop('checked', false);
+        // jQuery('.appliancePanel').show();
         
-        d3.select("#applianceList").selectAll("*").remove();
-        var buildingType = jQuery('input[name="hotelType"]:checked').val();
-        getAppliancePanel(dataList, buildingType);
+        // d3.select("#applianceList").selectAll("*").remove();
+        // var buildingType = jQuery('input[name="hotelType"]:checked').val();
+        // getAppliancePanel(dataList, buildingType);
     });
     
     jQuery('input[type=radio][name=restaurantType]').on('change', function(){
-        jQuery('input:checkbox').prop('checked', false);
-        jQuery('.appliancePanel').show();
+        // jQuery('input:checkbox').prop('checked', false);
+        // jQuery('.appliancePanel').show();
         
-        d3.select("#applianceList").selectAll("*").remove();
-        var buildingType = jQuery('input[name="restaurantType"]:checked').val();
-        getAppliancePanel(dataList, buildingType);
+        // d3.select("#applianceList").selectAll("*").remove();
+        // var buildingType = jQuery('input[name="restaurantType"]:checked').val();
+        // getAppliancePanel(dataList, buildingType);
     });
 
     jQuery('input[type=radio][name=shopType]').on('change', function(){
-        jQuery('input:checkbox').prop('checked', false);
-        jQuery('.appliancePanel').show();
+        // jQuery('input:checkbox').prop('checked', false);
+        // jQuery('.appliancePanel').show();
         
-        d3.select("#applianceList").selectAll("*").remove();
-        var buildingType = jQuery('input[name="shopType"]:checked').val();
-        getAppliancePanel(dataList, buildingType);
+        // d3.select("#applianceList").selectAll("*").remove();
+        // var buildingType = jQuery('input[name="shopType"]:checked').val();
+        // getAppliancePanel(dataList, buildingType);
+    });
+    */
+    jQuery('.leafRadio').on('change', function(){
+        jQuery('input:checkbox').prop('checked', false);
+        
+        var selectedVal = document.querySelector('input[name=' + this.name + ']:checked').value;
+        d3.json("static/data/buildingJson.json").then(function(data){
+            
+            // remove previous list
+            d3.select('.floorList').selectAll('*').remove();
+    
+            // select building
+            defaultBuilding = data["mra"];
+            
+            // get number of floors from the default building file
+            var nfloors = Object.keys(defaultBuilding["floors"]).length;
+            jQuery('#nfloors').val(nfloors);
+            
+            // add floors from the default file
+            var max_nzones = 0
+            for (var i=0; i<nfloors; i++) {
+                var floorPanel = d3.select('.floorList');
+                var nzones = Object.keys(defaultBuilding["floors"]["F" + (i+1).toString()]["zones"]).length;
+                if (nzones > max_nzones) { max_nzones = nzones; }
+                addFloor(i, floorPanel, nzones);
+            }
+            
+            // show configuration panel
+            jQuery('.configurationPanel').show();
+            
+            buildingModel = d3.select('.buildingModel')
+            generateTable(buildingModel, defaultBuilding, max_nzones);
+            
+            // show summary panel
+            jQuery('.summaryPanel').show();
+            
+            // add event on change in value of text box
+            jQuery('input[type=text][name=nfloors]').on('change', function(){
+                
+                // previous number of floors
+                prevNFloors = $(".floorList > div").length
+                
+                // update number of floors
+                nfloors = jQuery('input[type=text][name=nfloors]').val();
+                
+                // if now the number of floors is more
+                if (nfloors > prevNFloors) {
+                    // add new floors in the building
+                    for (var i=prevNFloors; i<nfloors; i++) {
+                        
+                        // add new floor
+                        var floorPanel = d3.select('.floorList');
+                        addFloor(i, floorPanel);
+                        
+                        // update building configuration json
+                        defaultBuilding["floors"]['F' + (i+1).toString()] = {};
+                        defaultBuilding["floors"]['F' + (i+1).toString()]["appliances"] = ["HVAC", "Lighting", "Laptops", "Monitors"];
+                        defaultBuilding["floors"]['F' + (i+1).toString()]["zones"] = {"Z1": ["HVAC", "Lighting", "Laptops", "Monitors"]};
+                    }
+                } // if now the number of floors is less
+                else if (nfloors < prevNFloors) {
+                    // remove floors from the end
+                    for (var i=prevNFloors; i>nfloors; i--) {
+                        
+                        // remove floor
+                        d3.select('.f' + i.toString()).remove();
+                        
+                        // update json
+                        delete defaultBuilding["floors"]['F' + (i).toString()];
+                    }
+                }
+                
+                // build summary table
+                generateTable(buildingModel, defaultBuilding, max_nzones);
+                console.log(defaultBuilding);
+            });
+            
+            // add event on change in value of text box
+            jQuery('input[type=text][name=nzones]').on('change', function(){
+                var floorID = this.id.split('zones')[0].toUpperCase();
+                var previousNZones = Object.keys(defaultBuilding["floors"][floorID]).length;
+                var nzonesNow = jQuery('#' + this.id).val();
+                
+                if (nzonesNow > max_nzones) { max_nzones = nzonesNow; }
+                if (nzonesNow > previousNZones) {
+                    for (var i=previousNZones; i<nzonesNow; i++) {
+                        defaultBuilding["floors"][floorID]["zones"]['Z' + (i+1).toString()] = ["HVAC", "Lighting", "Laptops", "Monitors"];
+                    }
+                } 
+                else if (nzonesNow < previousNZones) {
+                    // remove floors from the end
+                    for (var i=previousNZones; i>nzonesNow; i--) {
+                        delete defaultBuilding["floors"][floorID]["zones"]['Z' + (i).toString()];
+                    }
+                }
+                
+                // build summary table
+                generateTable(buildingModel, defaultBuilding, max_nzones);
+            });
+        });
     });
 });
 
